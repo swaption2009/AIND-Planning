e2c65bf0ee39e8663faa7b975971cb43334b4ef4
swaption2009 <swaption2009@gmail.com>
2017-04-21 18:10:21 -0700

    part 3 written analysis

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -293,7 +293,7 @@ def air_cargo_p2() -> AirCargoProblem:
 def air_cargo_p3() -> AirCargoProblem:
     # TODO implement Problem 3 definition
     cargos = ['C1', 'C2', 'C3', 'C4']
-    planes = ['P1', 'P2'] #done
+    planes = ['P1', 'P2']
     airports = ['JFK', 'SFO', 'ATL', 'ORD']
     pos = [expr('At(C1, SFO)'),
            expr('At(C2, JFK)'),
